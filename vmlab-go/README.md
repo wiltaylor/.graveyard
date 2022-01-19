4a649ece96167b4bb24bb4407b4f4cc2cce79d9b
Wil Taylor <cert@wiltaylor.dev>
2022-01-20 06:15:34 +1000

    Initial commit

---
+++ vmlab-go/README.md
@@ -0,0 +1,10 @@
+# vmlab
+Short description of what vmlab is, features etc.
+
+# Usage
+
+# Build
+
+# Contributions
+
+# License
---
+++ vmlab-go/shell.nix
@@ -0,0 +1,27 @@
+{ pkgs ? <nixpkgs> }:
+let
+
+  tmuxIde = pkgs.writeScriptBin "tmuxide" ''
+    tmux new-session -d -s vmlab vim
+    tmux rename-window 'neovim'
+    tmux select-window -t 'vmlab:0'
+    tmux split-window -v -p 30 zsh
+    tmux attach-session -t vmlab
+  '';
+in pkgs.mkShell {
+  name = "golangdevshell";
+  buildInputs = with pkgs; [
+    go
+    dep2nix
+    delve
+    tmuxIde
+  ];
+
+
+
+  shellHook = ''
+    echo "vmlab DevShell"
+    export SHELL=zsh
+    export EDITOR=vim
+  '';
+}
