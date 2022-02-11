fa6daa678ac4abbe5e7df3038785ce13243b6650
Wil Taylor <cert@wiltaylor.dev>
2022-02-11 21:26:11 +1100

    Added initial object creation tests

---
+++ interviewdemo/InterviewDemoApp.Core/NumberManager.cs
@@ -0,0 +1,13 @@
+﻿namespace InterviewDemoApp.Core;
+
+public class NumberManager
+{
+    public NumberManager(int interval)
+    {
+        if (interval < 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(interval), 
+                "Expected a time interval larger than 0 seconds");
+        } 
+    }
+}
---
+++ interviewdemo/InterviewDemoapp.Test/NumberManagerTest.cs
@@ -0,0 +1,22 @@
+﻿using InterviewDemoApp.Core;
+using Xunit;
+
+namespace InterviewDemoApp.Test;
+
+public class NumberManagerTest
+{
+    [Fact]
+    public void When_CreatingWithPositiveTimeInterval_Should_NotThrow()
+    {
+        var sut = new NumberManager(5);
+    }
+
+    [Fact]
+    public void When_CreatingWithNegativeInterval_Should_Throw()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            var sut = new NumberManager(-5);
+        });
+    }
+}
